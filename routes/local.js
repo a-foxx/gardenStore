7b4f1542b890b74159f4f0bf065ea9343bb650c8
Mister Fox <alanfox70@gmail.com>
2023-08-23 00:03:43 +0100

    added header to register endpoint

--- routes/local.js
+++ routes/local.js
@@ -7,6 +7,7 @@ router.post(
   "/signup",
     passport.authenticate("local-signup", {failureFlash: true}),
     (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://garden-store-frontend.vercel.app');
     res.json({
       user: req.user,
     });
