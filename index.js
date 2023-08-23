4a33b889c8f93823484a1b611673dad81b2f02e6
Mister Fox <alanfox70@gmail.com>
2023-08-23 23:51:18 +0100

    changes session cofig

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false, // don't save session if unmodified
   saveUninitialized: false, // don't create session until something stored
-  cookie: { secure: false },
+  cookie: { secure: true },
   store: store
 }));
 
